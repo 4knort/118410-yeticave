e42cb4456f9db52769464046fd96dd488278a6d5
Nort <4knort@gmail.com>
2017-05-05 02:21:14 +0300

    added redirect

--- add.php
+++ add.php
@@ -1,34 +1,51 @@
 <?php 
   require 'functions.php';
+  require 'lots.php';
+
   $formClass = 'form form--add-lot container';
   $errorArr = [];
-  $value = '';
-  $errors = false;
+  if(count($_POST) > 0) {
 
-  if ($_POST['lot-name'] == $value) {
-    $errorArr['lot-name'] = 'enter lot name';
-    $errorArr['lot-name-class'] = 'form__item--invalid';
-  }
+    $value = '';
+    $errors = false;
 
-  if ($_POST['lot-rate'] <= 0) {
-    $errorArr['lot-rate'] = 'enter lot rate';
-    $errorArr['lot-rate-class'] = 'form__item--invalid';
-  }
-  if ($_POST['lot-step'] <= 0) {
-    $errorArr['lot-step'] = 'enter lot step';
-    $errorArr['lot-step-class'] = 'form__item--invalid';
-  }
-
-
-  foreach ($errorArr as $key => $value) {
-    if ($value != '') {
-      $errors = true;
+    if ($_POST['lot-name'] == $value) {
+      $errorArr['lot-name'] = 'enter lot name';
+      $errorArr['lot-name-class'] = 'form__item--invalid';
     }
-  }
-  
-  if(!$errors) {
-    $name = basename($_FILES["add-lot-image"]["name"]);
-    move_uploaded_file($_FILES['add-lot-image']['tmp_name'], 'img/' . $name);
+
+    if ($_POST['lot-rate'] <= 0) {
+      $errorArr['lot-rate'] = 'enter lot rate';
+      $errorArr['lot-rate-class'] = 'form__item--invalid';
+    }
+    if ($_POST['lot-step'] <= 0) {
+      $errorArr['lot-step'] = 'enter lot step';
+      $errorArr['lot-step-class'] = 'form__item--invalid';
+    }
+
+
+    foreach ($errorArr as $key => $value) {
+      if ($value != '') {
+        $errors = true;
+      }
+    }
+
+    if(!$errors) {
+      $formClass = $formClass . ' form--invalid';
+      $name = basename($_FILES["add-lot-image"]["name"]);
+      move_uploaded_file($_FILES['add-lot-image']['tmp_name'], 'img/' . $name);
+
+      $newLot = [
+        'name' => $_POST['lot-name'], 
+        'categorie' => $_POST['category'], 
+        'price' => $_POST['lot-rate'],
+        'img' => 'img/' . $name,
+        'description' => $_POST['message'],
+      ];
+      array_push($lots, $newLot);
+      $newLotsId = count($lots) - 1;
+      header("Location: lot.php?id=$newLotsId");
+    }
   }
 ?>
 
