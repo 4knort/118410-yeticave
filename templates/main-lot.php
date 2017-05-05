0de797f512beaaa78c9a396efe953db86252db11
Nort <4knort@gmail.com>
2017-05-06 00:54:35 +0300

    fixed category bug

--- templates/main-lot.php
+++ templates/main-lot.php
@@ -28,7 +28,7 @@
                 <div class="lot-item__image">
                     <img src="<?=$data['currentLot']['img'] ;?>" width="730" height="548" alt="Сноуборд">
                 </div>
-                <p class="lot-item__category">Категория: <span><?=$data['currentLot']['name'] ;?></span></p>
+                <p class="lot-item__category">Категория: <span><?=$data['currentLot']['categorie'] ;?></span></p>
                 <p class="lot-item__description"><?=$data['currentLot']['description'] ;?></p>
             </div>
             <div class="lot-item__right">
